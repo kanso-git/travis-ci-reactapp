e47fe27b7ca0790cc21aa63f48f19759d9ea22d9
kansoa <abdallah.kanso@unine.ch>
2020-09-08 17:01:39 +0200

    change on branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. now 6
+          Edit <code>src/App.tsx</code> and save to reload. now changes from feature branch
         </p>
         <a
           className="App-link"
